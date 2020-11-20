4b700320dcb9cd6e43fa31164022540dcb7334f8
Leon <qaz@dds.nl>
2020-11-20 20:18:05 +0100

    Added start.spring.io project and merged in the spring-jdbc code

---
+++ boot-jdbc/src/main/java/nl/leonw/demo1/bootjdbc/BootJdbcApplication.java
@@ -0,0 +1,25 @@
+package nl.leonw.demo1.bootjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.sql.ResultSet;
+
+@SpringBootApplication
+public class BootJdbcApplication {
+
+	public static void main(String[] args) {
+		var ctx = SpringApplication.run(BootJdbcApplication.class, args);
+
+		// Do something just to prove it does something
+		var jdbc = ctx.getBean(JdbcTemplate.class);
+		jdbc.query("select feature_name from information_schema.sql_features where is_supported = 'YES'",
+				(ResultSet rs) -> System.out.println(rs.getString("feature_name")));
+
+		System.out.println("------The End--------" );
+
+	}
+
+}
---
+++ boot-jdbc/src/main/java/nl/leonw/demo1/bootjdbc/Config.java
@@ -0,0 +1,14 @@
+package nl.leonw.demo1.bootjdbc;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+
+    // This is empty
+    // The beans we made in the previous project?
+
+    // Just having the maven dependency om spring data JDBC tells Spring we want a datasource and template
+
+    // See JdbcTemplateAutoConfiguration
+}
