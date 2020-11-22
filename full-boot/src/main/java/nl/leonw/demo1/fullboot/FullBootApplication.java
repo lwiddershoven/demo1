d81ffc637f1e64593ad4b7aa1583bb665ee68811
Leon <qaz@dds.nl>
2020-11-22 17:05:12 +0100

    Full spring boot app with liquibase and hikari

---
+++ full-boot/src/main/java/nl/leonw/demo1/fullboot/FullBootApplication.java
@@ -0,0 +1,13 @@
+package nl.leonw.demo1.fullboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullBootApplication.class, args);
+	}
+
+}
