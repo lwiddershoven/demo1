b9c129ba47125d91e1f40184e08cea352c7a7e0b
Leon <qaz@dds.nl>
2020-11-23 09:10:23 +0100

    Added application description

--- full-boot/src/main/java/nl/leonw/demo1/fullboot/FullBootApplication.java
+++ full-boot/src/main/java/nl/leonw/demo1/fullboot/FullBootApplication.java
@@ -3,6 +3,19 @@ package nl.leonw.demo1.fullboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+// Full application:
+// DB: Hikari Connection Pool
+// DB: Liquibase schema management
+// DB: Generated repositories
+// REST: API
+// REST: JSON support
+// REST: SpringFox Swagger (OpenAPI) support
+// REST: Spring security secured endpoints
+// Insight: Actuator insight in application
+// Insight: micrometer/prometheus metrics
+
+
 @SpringBootApplication
 public class FullBootApplication {
 
